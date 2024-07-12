6e138db9b2536a9200df953915d9c3c4d344029c
minh <nh.minh0403@gmail.com>
2024-07-12 20:12:11 +0700

    observer +3

--- ObserverPattern/ISubject.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-interface Subject
-{
-    public function registerObserver(Observer $observer);
-    public function removeObserver(Observer $observer);
-    public function notifyObservers();
-}
--- ObserverPattern/index.php
+++ ObserverPattern/index.php
@@ -1,23 +1,74 @@
 <?php
 
-require_once './ISubject.php';
-require_once './IObserver.php';
-require_once './NewsPublisher.php';
-require_once './NewSubcriber.php';
+interface Observer
+{
+  public function update($title, $content);
+}
+
+interface Subject
+{
+  public function registerObserver(Observer $observer);
+  public function removeObservers(Observer $observer);
+  public function notifyObservers();
+}
 
 
-//create a new publisher
-$publisher = new NewsPublisher();
+class NewPublisher implements Subject
+{
+  private $observers = [];
+  private $title;
+  private $content;
 
+  public function registerObserver(Observer $observer)
+  {
+    $this->observers[] = $observer;
+  }
 
-//create new subcribers
+  public function removeObservers(Observer $observer)
+  {
+    $key = array_search($observer, $this->observers);
+    if ($key !== false) {
+      unset($this->observers[$key]);
+    }
+  }
+
+  public function notifyObservers()
+  {
+    foreach ($this->observers as $observer) {
+      $observer->update($this->title, $this->content);
+    }
+  }
+
+  public function publicNews($title, $content)
+  {
+    $this->title = $title;
+    $this->content = $content;
+    $this->notifyObservers();
+  }
+}
+
+class NewSubcriber implements Observer
+{
+  private $name;
+
+  public function __construct($name)
+  {
+    $this->name = $name;
+  }
+
+  public function update($title, $content)
+  {
+    echo "Hello {$this->name}, a new article has been published: {$title}\n";
+    echo "Content: {$content}\n";
+  }
+}
+
+$publisher = new NewPublisher();
+
 $subcriber1 = new NewSubcriber("Minh");
 $subcriber2 = new NewSubcriber("Ly");
 
-
-//register subcriber
 $publisher->registerObserver($subcriber1);
 $publisher->registerObserver($subcriber2);
 
-//publish news
-$publisher->publishNews("Breaking news", "This is the content of the breaking news!");
+$publisher->publicNews('Breaking News', 'This isthe content of the breaking news');
