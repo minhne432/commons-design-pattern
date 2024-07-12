a900fd4155738905893e70c6b9eab532aef6dd6a
minh <nh.minh0403@gmail.com>
2024-07-12 19:24:59 +0700

    singleton +5

--- SingletonPattern/index.php
+++ SingletonPattern/index.php
@@ -1,18 +1,15 @@
 <?php
 
-
 class Database
 {
   private static $instance = null;
   private $connection;
 
-  //private constructor to prevent direct creation of objects
   private function __construct()
   {
     $this->connection = new PDO('mysql:host=localhost;dbname=test', 'root', '');
   }
 
-  //public static method to provide access to the single instance
   public static function getInstance()
   {
     if (self::$instance == null) {
@@ -21,12 +18,10 @@ class Database
     return self::$instance;
   }
 
-  //prevent cloning of the instance
   private function __clone()
   {
   }
 
-  //prevent unserializtion of the instance
   private function __wakeup()
   {
   }
@@ -36,18 +31,3 @@ class Database
     return $this->connection;
   }
 }
-
-
-//get the single instance of the Database class
-$db = Database::getInstance();
-
-//get PDO connection
-$connection = $db->getConnection();
-
-//use the connection to execute queries
-$query = $connection->query('SELECT * FROM users');
-
-$result = $query->fetchAll(PDO::FETCH_ASSOC);
-
-
-print_r($result);
