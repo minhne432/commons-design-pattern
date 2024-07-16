8c9535a7fb4819a34d918b2ddc3d7649e50a2959
minh <nh.minh0403@gmail.com>
2024-07-16 11:09:59 +0700

    SetterInjection +1

--- DependencyInjection/SetterInjection/lab1.php
+++ DependencyInjection/SetterInjection/lab1.php
@@ -1,1 +1,58 @@
 <?php
+
+class Logger
+{
+  public function log($message)
+  {
+    echo "Log message: $message\n";
+  }
+}
+
+
+class Mailer
+{
+  public function send($recipient, $message)
+  {
+    echo "Sending email to $recipient: $message\n";
+  }
+}
+
+
+class UserService
+{
+  private $logger;
+  private $mailer;
+
+  public function setLogger(Logger $logger)
+  {
+    $this->logger = $logger;
+  }
+
+  public function setMailer(Mailer $mailer)
+  {
+    $this->mailer = $mailer;
+  }
+
+  public function registerUser($email)
+  {
+    if ($this->logger) {
+      $this->logger->log("User registered with email: $email");
+    }
+
+    if ($this->mailer) {
+      $this->mailer->send($email, "Welcome to our service!");
+    }
+  }
+}
+
+
+
+$userService = new UserService();
+
+$logger = new Logger();
+$mailer = new Mailer();
+
+$userService->setLogger($logger);
+$userService->setMailer($mailer);
+
+$userService->registerUser("user@example.com");
